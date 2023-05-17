5f8f58c2825b13dbfc506240a1c57ed4f61f7904
roivasxxx <tiragoshi@gmail.com>
2023-05-17 18:02:47 +0200

    sdsd

--- src/components/hiddenTask.jsx
+++ src/components/hiddenTask.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 
 export default function HiddenTask({ isHiddenText, children }) {
   const [isHidden, setIsHidden] = useState(true);
